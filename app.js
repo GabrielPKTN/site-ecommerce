0df007a55b3257b685a2776b6c78e3faa85b3b57
GABRIEL LACERDA CORREIA <25132902@CFP127ALUNO.LOCAL>
2025-08-26 10:26:33 -0300

    tornando o slider de produtos funcional

--- app.js
+++ app.js
@@ -13,11 +13,13 @@ function criarProduto(jsonProduto) {
 
     const produto = document.createElement('div')
     const imagemProduto = document.createElement('img')
+    const divDescricao = document.createElement('div')
     const nomeProduto = document.createElement('label')
     const descricaoProduto = document.createElement('p')
     const precoProduto = document.createElement('span')
 
     produto.classList.add('produto')
+    divDescricao.classList.add('container-descricao')
 
     imagemProduto.src = jsonProduto.imagem
     nomeProduto.textContent = jsonProduto.nome
@@ -26,11 +28,39 @@ function criarProduto(jsonProduto) {
 
     containerProduto.appendChild(produto)
     produto.appendChild(imagemProduto)
-    produto.appendChild(nomeProduto)
-    produto.appendChild(descricaoProduto)
-    produto.appendChild(precoProduto)
+    produto.appendChild(divDescricao)
+    divDescricao.appendChild(nomeProduto)
+    divDescricao.appendChild(descricaoProduto)
+    divDescricao.appendChild(precoProduto)
 
 }
 
-
 carregarProdutos()
+
+const buttonPrevious = document.getElementById('previous')
+
+let produtos = document.querySelectorAll('.produto')
+
+function previous() {
+
+    const containerProduto = document.getElementById('container-produtos')
+
+    containerProduto.appendChild(produtos[0])
+    produtos = document.querySelectorAll('.produto')
+
+}
+
+const buttonNext = document.getElementById('next') 
+
+function next() {
+
+    const containerProduto = document.getElementById('container-produtos')
+
+    const lastProduto = produtos[produtos.length -1]
+    containerProduto.insertBefore(lastProduto, produtos[0])
+    produtos = document.querySelectorAll('.produto')
+
+}
+
+buttonPrevious.addEventListener('click', previous)
+buttonNext.addEventListener('click', next)
